6f89920313d88cdf010a787410cc1c8af80aebd5
Naohiro Sakuma <nao.bear@gmail.com>
2019-12-22 19:09:04 +0900

    reading yaml file

--- main.go
+++ main.go
@@ -1,30 +1,67 @@
 package main
 
 import (
-	"os"
+	"fmt"
+	"io/ioutil"
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/sakuma/aws-sitter/lib/holiday"
 	ec2controller "github.com/sakuma/aws-sitter/aws/ec2"
+	"github.com/sakuma/aws-sitter/lib/holiday"
+	"gopkg.in/yaml.v2"
 )
 
 func isActive(t time.Time) bool {
 	return holiday.IsHoliday()
 }
 
+type Instance struct {
+	Region string `toml:region`
+	Type   string `toml:type`
+	Name   string `toml:name`
+	ID     string `toml:id`
+}
+
+func ReadInstanceFile(path string) ([]Instance, error) {
+	// TODO: error handling
+	buf, _ := ioutil.ReadFile("./instances.yml")
+	data := make([]Instance, 20)
+
+	// TODO: error handling
+	err := yaml.Unmarshal(buf, &data)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return data, nil
+}
+
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler() (string, error) {
-	instanceID := os.Getenv("INSTANCE_ID")
+	fmt.Println("call Handler")
+
 	t := time.Now()
 	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
 	current := t.In(jst)
-	if isActive(current) {
-		ec2controller.StartInstance(instanceID)
-		return "succedked: start instance.", nil
+
+	// TODO: error handling
+	instances, _ := ReadInstanceFile("./instances.yml")
+	for _, s := range instances {
+		fmt.Println("Region: ", s.Region)
+		fmt.Println("Type: ", s.Type)
+		fmt.Println("Name: ", s.Name)
+		fmt.Println("ID: ", s.ID)
+
+
+		if isActive(current) {
+			ec2controller.StartInstance(s.ID)
+			fmt.Println("succeeded: start instance.")
+		} else {
+			ec2controller.StopInstance(s.ID)
+			fmt.Println("succeeded: stop instance.")
+		}
 	}
-	ec2controller.StopInstance(instanceID)
-	return "succeeded: stop instance.", nil
+	return "succeded process", nil
 }
 
 func main() {
