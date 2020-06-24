def375a51720e6f4a5dd40b21bfe92b288b8fa0d
Naohiro Sakuma <nao.bear@gmail.com>
2020-06-24 22:44:07 +0900

    Refactor: using testify/asset

--- sitter/aws_test.go
+++ sitter/aws_test.go
@@ -3,136 +3,81 @@ package sitter
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIsWithinScheduleTime(t *testing.T) {
-
 	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
 	var tt time.Time
-	var got bool
+
 	i := Instance{RunSchedule: "10-19"}
 
 	// out of range(7:00)
 	tt = time.Date(2019, 8, 31, 7, 00, 0, 0, jst)
-	got = i.isWithinScheduleTime(tt)
-	if got != false {
-		t.Fatal("got: true, should be false")
-	}
+	assert.Equal(t, i.isWithinScheduleTime(tt), false)
 
 	// in time (10:00)
 	tt = time.Date(2019, 8, 31, 10, 00, 0, 0, jst)
-	got = i.isWithinScheduleTime(tt)
-	if got != true {
-		t.Fatal("got: false, should be true")
-	}
+	assert.Equal(t, i.isWithinScheduleTime(tt), true)
 
 	// in time (10:01)
 	tt = time.Date(2019, 8, 31, 10, 01, 0, 0, jst)
-	got = i.isWithinScheduleTime(tt)
-	if got != true {
-		t.Fatal("got: false, should be true")
-	}
+	assert.Equal(t, i.isWithinScheduleTime(tt), true)
 
 	// in time (19:59)
 	tt = time.Date(2019, 8, 31, 19, 59, 0, 0, jst)
-	got = i.isWithinScheduleTime(tt)
-	if got != true {
-		t.Fatal("got: false, should be true")
-	}
+	assert.Equal(t, i.isWithinScheduleTime(tt), true)
 
 	// out of range (20:00)
 	tt = time.Date(2019, 8, 31, 20, 00, 0, 0, jst)
-	got = i.isWithinScheduleTime(tt)
-	if got != false {
-		t.Fatal("got: true, should be false")
-	}
+	assert.Equal(t, i.isWithinScheduleTime(tt), false)
 
 	// out of range (20:01)
 	tt = time.Date(2019, 8, 31, 20, 01, 0, 0, jst)
-	got = i.isWithinScheduleTime(tt)
-	if got != false {
-		t.Fatal("got: true, should be false")
-	}
+	assert.Equal(t, i.isWithinScheduleTime(tt), false)
 }
 
 func TestIsRunning(t *testing.T) {
-	var got bool
 	i := Instance{}
 
 	i.State = "running"
-	got = i.isRunning()
-	if got != true {
-		t.Fatal("got: false, should be true")
-	}
+	assert.Equal(t, i.isRunning(), true)
 
 	i.State = "available"
-	got = i.isRunning()
-	if got != true {
-		t.Fatal("got: false, should be true")
-	}
+	assert.Equal(t, i.isRunning(), true)
 
 	i.State = "stopping"
-	got = i.isRunning()
-	if got != false {
-		t.Fatal("got: true, should be false")
-	}
+	assert.Equal(t, i.isRunning(), false)
 
 	i.State = "stopped"
-	got = i.isRunning()
-	if got != false {
-		t.Fatal("got: true, should be false")
-	}
+	assert.Equal(t, i.isRunning(), false)
 
 	i.State = "shutting"
-	got = i.isRunning()
-	if got != false {
-		t.Fatal("got: true, should be false")
-	}
+	assert.Equal(t, i.isRunning(), false)
 
 	i.State = "terminated"
-	got = i.isRunning()
-	if got != false {
-		t.Fatal("got: true, should be false")
-	}
+	assert.Equal(t, i.isRunning(), false)
 
 	i.State = "hoge"
-	got = i.isRunning()
-	if got != false {
-		t.Fatal("got: true, should be false")
-	}
+	assert.Equal(t, i.isRunning(), false)
 }
 
 func TestIsStopped(t *testing.T) {
-	var got bool
 	i := Instance{}
 
 	i.State = "stopped"
-	got = i.isStopped()
-	if got != true {
-		t.Fatal("got: false, should be true")
-	}
+	assert.Equal(t, i.isStopped(), true)
 
 	i.State = "stopping"
-	got = i.isStopped()
-	if got != true {
-		t.Fatal("got: false, should be true")
-	}
+	assert.Equal(t, i.isStopped(), true)
 
 	i.State = "shutting"
-	got = i.isStopped()
-	if got != true {
-		t.Fatal("got: false, should be true")
-	}
+	assert.Equal(t, i.isStopped(), true)
 
 	i.State = "terminated"
-	got = i.isStopped()
-	if got != true {
-		t.Fatal("got: false, should be true")
-	}
+	assert.Equal(t, i.isStopped(), true)
 
 	i.State = "running"
-	got = i.isStopped()
-	if got != false {
-		t.Fatal("got: true, should be false")
-	}
+	assert.Equal(t, i.isStopped(), false)
 }
